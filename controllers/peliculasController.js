82b4148febd18e935d411919cdc3ea119d8467e6
maxii73 <athousandsuns73@gmail.com>
2022-10-15 21:42:05 -0300

    add controllers and routes

---
+++ controllers/peliculasController.js
@@ -0,0 +1,12 @@
+let db = require ('../database/models');
+
+let peliculasController = {
+    crear: function (req,res) {
+        db.Genero.findAll()
+            .then(function(generos){
+                return res.render('listadoPeliculas', {generos:generos})
+            })        
+    }
+}
+
+module.exports= peliculasController;
