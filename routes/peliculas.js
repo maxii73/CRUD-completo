0244dcebdc6054491e7769e434c384bceb4f15b4
maxii73 <athousandsuns73@gmail.com>
2022-10-18 23:05:13 -0300

    termine create

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -4,6 +4,11 @@ var peliculasController= require ('../controllers/peliculasController')
 
 //Creacion
 
-router.get('/crear', peliculasController.crear)
+router.get('/crear', peliculasController.crear);
+router.post('/crear', peliculasController.guardado);
+
+//Lectura
+
+router.get('/',peliculasController.listado);
 
 module.exports = router;
